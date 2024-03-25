43d4461c2840c3e2995ec6654d42169697acd228
hd702 <haru.doyu@gmail.com>
2024-03-25 13:02:36 +0200

    clean test functions

--- test_all.py
+++ test_all.py
@@ -14,100 +14,66 @@ def serialize_fp32(file, tensor):
     b = struct.pack(f'{len(d)}f', *d)
     file.write(b)
 
-def test_linear():
-    inp = 4096
-    out = 4096
-
-    fc = nn.Linear(inp, out, bias=True)
-
-    model = nn.Sequential(*[fc for _ in range(1000)])
+def save_model_and_data(data, model):
     f = open("model.pt", "wb")
     for p in model.parameters():
         serialize_fp32(f, p)
     f.close()
 
-    data = torch.rand(inp)
     f = open("data.txt", "wb")
     serialize_fp32(f, data)
     f.close()
 
-    # measure the time it takes to perform linear operations 
-    # Python
-    start_time = time.time()
-    ref = model(data).detach().numpy()
-    print("Python: {:.3f} seconds".format(time.time() - start_time))
-
-    # write outputs C to make sure later the results match
+def execute_commands(commands):
     f = open("stdout.txt", "wb")
-
-    # C: Four execution with and without OpenMP and BLAS support
-    commands = ["./run-linear", ["./run-linear", "blas"], "./run-linear-p", ["./run-linear-p", "blas"]]
     durations = []
-    for command in commands:
+    for cmd in commands:
         start_time = time.time()
-        proc = subprocess.Popen(command, stdout=f, stderr=subprocess.PIPE)
+        proc = subprocess.Popen(cmd, stdout=f, stderr=subprocess.PIPE)
         proc.wait()
         duration = time.time() - start_time
         durations.append(duration)
-        print("{}: {:.3f} seconds".format(command, duration))
-
     f.close()
 
-    # ensure the results match
-    res = np.loadtxt("stdout.txt").reshape(4, out)
-    for i in range(4):
-        assert np.allclose(res[i], ref, rtol=1e-05, atol=1e-06) # Python and C non-optimized
+    return durations
 
+def calculate_time(data, model, commands):
+    save_model_and_data(data, model)
+    start_time = time.time()
+    ref = model(data).detach().numpy().flatten()
+    print("Python: {:.3f} seconds".format(time.time() - start_time))
+
+    # C: Four execution with and without OpenMP and BLAS support
+    durations = execute_commands(commands)
+
+    # check results match reference values
+    res = np.loadtxt("stdout.txt").reshape(4, len(ref))
+    for i in range(4):
+        assert np.allclose(res[i], ref, rtol=1e-05, atol=1e-06)
+
+    # display durations
     df = pd.DataFrame(np.array(durations).reshape(2,2)).round(3)
     df.columns = ["Without BLAS", "With BLAS"]
     df.index = ["Without OpenMP", "With OpenMP"]
-    print()
-    print(df)
+    print(df, "\n")
+
+def test_linear():
+    dim = 4096
+    data = torch.rand(dim)
+    fc = nn.Linear(dim, dim, bias=True)
+    model = nn.Sequential(*[fc for _ in range(100)])
+    # C: Four execution with and without OpenMP and BLAS support
+    commands = ["./run-linear", ["./run-linear", "blas"], "./run-linear-p", ["./run-linear-p", "blas"]]
+    calculate_time(data, model, commands)
 
 def test_conv():
-    conv_in = nn.Conv2d(3, 64, 3, 1, 1, bias=True)
-    conv = nn.Conv2d(64, 64, 3, 1, 1, bias=True)
-
+    nch_in, h, w = 3, 28, 28
+    nch_hid, ks, st, pad = 64, 3, 1, 1
+    data = torch.rand(nch_in * h * w).view(1, nch_in, h, w)
+    conv_in = nn.Conv2d(nch_in, nch_hid, ks, st, pad, bias=True)
+    conv = nn.Conv2d(nch_hid, nch_hid, ks, st, pad, bias=True)
     model = nn.Sequential(conv_in, *[conv for _ in range(100)])
-    f = open("model.pt", "wb")
-    for p in model.parameters():
-        serialize_fp32(f, p)
-    f.close()
-
-    data = torch.rand(3 * 28 * 28).view(1, 3, 28, 28)
-    f = open("data.txt", "wb")
-    serialize_fp32(f, data)
-    f.close()
-
-    # measure the time it takes to perform convolutional operations 
-    # Python
-    start_time = time.time()
-    ref = model(data).detach().numpy()
-    print("Python: {:.3f} seconds".format(time.time() - start_time))
-
-    # write outputs C to make sure later the results match
-    f = open("stdout.txt", "wb")
 
     # C: Four execution with and without OpenMP and BLAS support
     commands = ["./run-conv", ["./run-conv", "blas"], "./run-conv-p", ["./run-conv-p", "blas"]]
-    durations = []
-    for command in commands:
-        start_time = time.time()
-        proc = subprocess.Popen(command, stdout=f, stderr=subprocess.PIPE)
-        proc.wait()
-        duration = time.time() - start_time
-        durations.append(duration)
-        print("{}: {:.3f} seconds".format(command, duration))
-
-    f.close()
-
-    # ensure the results match
-    res = np.loadtxt("stdout.txt").reshape(4, 64, 28, 28)
-    for i in range(4):
-        assert np.allclose(res[i], ref, rtol=1e-05, atol=1e-06) # Python and C non-optimized
-
-    df = pd.DataFrame(np.array(durations).reshape(2,2)).round(3)
-    df.columns = ["Without BLAS", "With BLAS"]
-    df.index = ["Without OpenMP", "With OpenMP"]
-    print()
-    print(df)
+    calculate_time(data, model, commands)
